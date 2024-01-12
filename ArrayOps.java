d420a58d1570cc36b105962954974a5d6c5953f1
omerlasry <154352687+omerlasry@users.noreply.github.com>
2024-01-12 23:15:43 +0200

    fixed program

--- ArrayOps.java
+++ ArrayOps.java
@@ -3,24 +3,76 @@ public class ArrayOps {
         
     }
     
-    public static int findMissingInt (int [] array) {
+	public static int findMissingInt (int [] array) 
+	{
         // Write your code here:
-        return -1;
+		if(array.length==1)
+			return 1;
+		boolean test = false;
+		for(int i =0 ; i<array.length ; i++)
+		{
+			test = false;
+			for(int j= 0 ; j<array.length;j++)
+				if(array[j]==i)
+					test = true;
+			if(test==false)
+				return i;
+		}
+		return -1;
+		
     }
 
-    public static int secondMaxValue(int [] array) {
+
+    public static int secondMaxValue(int [] array) 
+	{
         // Write your code here:
-        return 0;
+		int temp = array[0];
+		int max = array[0];
+		for(int i = 0 ; i <array.length ; i++)
+		{
+			if(array[i]>max)
+				max = array[i];
+		}
+		for(int i = 0 ; i<array.length;i++)
+			if(array[i]>temp && array[i]!=max)
+				temp = array[i];
+			
+        return temp;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static boolean containsTheSameElements(int [] array1,int [] array2)
+	{
         // Write your code here:
-        return false;
-    }
+		boolean t = false;
+		for(int i = 0 ; i<array1.length ; i++)
+		{
+			t = false;
+			for(int j = 0 ; j<array2.length ; j++)
+				if(array1[i]==array2[j])
+					t = true;
+			if(t==false)
+				return false;
+			
+		}
+		return true;
+	}
 
-    public static boolean isSorted(int [] array) {
+    public static boolean isSorted(int [] array) 
+	{
         // Write your code here:
-        return false;
+		if(array[0]>array[1])
+		{
+			for(int i = 2 ; i<array.length-1;i++)
+			{
+				if(array[i]>array[i+1])
+					return false;
+			}
+		}
+		for(int i = 2 ; i<array.length-1;i++)
+			if(array[i]<=array[i+1])
+				return false;
+		return true;	
+
     }
 
 }
