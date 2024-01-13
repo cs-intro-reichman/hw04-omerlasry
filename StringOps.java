991d228a513a0861589b3001ce105f44f9b4ce07
omerlasry <154352687+omerlasry@users.noreply.github.com>
2024-01-13 12:19:30 +0200

    fixed program

--- StringOps.java
+++ StringOps.java
@@ -50,9 +50,29 @@ public class StringOps {
         return ans;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase (String s) 
+	{
+        // One two tH:
+		String ans = "" ;
+		for(int i = 0 ; i<s.length() ; i++)
+		{
+			char ch = s.charAt(i);
+			if(ch>=65&&ch<=90)
+				ans = ans +(char)(s.charAt(i)+32);
+			else if(ch!=32)
+				ans = ans +(char)(s.charAt(i));	
+			if(ch == 32)
+			{
+				if(s.charAt(i+1)>=97 && s.charAt(i+1)<=122)
+					ans = ans +(char)(s.charAt(i+1)-32);
+				else
+					ans = ans +(char)(s.charAt(i+1));
+				i++;		
+			}	
+		
+		}
+			
+        return ans;
     }
 
     public static int[] allIndexOf (String string, char chr) {
